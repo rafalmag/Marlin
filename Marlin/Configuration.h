da592299a5fab98374451ce164989763165ec261
Rafal Magda <rafalmag@gmail.com>
2019-11-17 10:50:10 +0100

    extrude length fix + HYBRID_THRESHOLD

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -565,7 +565,7 @@
  * Note: For Bowden Extruders make this large enough to allow load/unload.
  */
 #define PREVENT_LENGTHY_EXTRUDE
-#define EXTRUDE_MAXLENGTH 200
+#define EXTRUDE_MAXLENGTH 400
 
 //===========================================================================
 //======================== Thermal Runaway Protection =======================
