0572fa71d6c22425b491d38a49cbeba469acd540
Scott Lahteine <github@thinkyhead.com>
2020-04-03 19:49:45 -0500

    Add millis helper macros

--- Marlin/src/core/millis_t.h
+++ Marlin/src/core/millis_t.h
@@ -25,5 +25,9 @@
 
 typedef uint32_t millis_t;
 
+#define MIN_TO_MS(N) millis_t((N)*60UL*1000UL)
+#define MS_TO_SEC(N) millis_t((N)/1000UL)
+#define SEC_TO_MS(N) SEC_TO_MS((N)*60UL)
+
 #define PENDING(NOW,SOON) ((int32_t)(NOW-(SOON))<0)
 #define ELAPSED(NOW,SOON) (!PENDING(NOW,SOON))
